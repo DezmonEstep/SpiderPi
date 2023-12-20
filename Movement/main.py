25eaa69d86a83a14b2acdbf886e2d9391df7ebd4
DezmonEstep <99703450+DezmonEstep@users.noreply.github.com>
2023-12-20 16:27:42 -0500

    Update main.py

--- Movement/main.py
+++ Movement/main.py
@@ -1,23 +1,22 @@
 import Hexapod
 import time
 
-def main():
-   # Initialize the hexapod
-   Hexapod.load()
 
-   # Move each leg forward
-   for leg in range(6):
-       Hexapod.forward_step(leg)
+# Initialize the hexapod
+Hexapod.load()
 
-   # Wait for a while
-   time.sleep(2)
+# Move each leg forward
+for leg in range(6):
+    Hexapod.forward_step(leg)
+
+# Wait for a while
+time.sleep(2)
 
    # Move each leg backward
-   for leg in range(6):
-       Hexapod.backward_step(leg)
+for leg in range(6):
+    Hexapod.backward_step(leg)
 
-   # Unload the hexapod
-   Hexapod.unload()
+# Unload the hexapod
+Hexapod.unload()
 
-if __name__ == "__main__":
-   main()
+
